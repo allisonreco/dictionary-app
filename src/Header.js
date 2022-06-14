fe96a727ac9caf0b92cbbcabbf82e3469004d91f
Allison <allison.reco@gmail.com>
2022-06-14 17:39:14 +0200

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <div>
+        <img src="images/dictionary.svg" alt="Logo" width="70px" />
+        <h1>Dictionary</h1>
+      </div>
+    </div>
+  );
+}
