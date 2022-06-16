7a5419a6904cb2a8a0d9635d50dc255444dd8d34
Allison <allison.reco@gmail.com>
2022-06-16 11:25:02 +0200

    add dictionary api integration

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function Search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
