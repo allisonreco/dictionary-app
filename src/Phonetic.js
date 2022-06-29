8452d80a00fc9cc92053a0526f8a7c1eb2161b64
Allison <allison.reco@gmail.com>
2022-06-29 09:41:26 +0200

    add audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Phonetic.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
 export default function Phonetic(props) {
+  const audio = new Audio(props.phonetic.audio);
+
+  function playAudio() {
+    audio.play();
+  }
+
   return (
     <div className="Phonetic">
       <p>
         [{props.phonetic.text}] &nbsp;
-        <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+        <a href="#" rel="noreferrer" onClick={playAudio}>
           <FontAwesomeIcon icon={faVolumeHigh} />
         </a>
       </p>
